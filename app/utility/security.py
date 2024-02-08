4a178701ae05efbb12c3cc2d8011c20a7bf4dd08
Kishor Ramanan <kishorramanan5@gmail.com>
2024-02-08 18:53:25 +0530

    Removed Magic
    libmagic is not installed on Railway

--- app/utility/security.py
+++ app/utility/security.py
@@ -1,10 +1,11 @@
-import magic
+# import magic
 
 ALLOWED_IMAGE_EXTENSIONS = (".jpg", ".png", ".jpeg")
 
 
 def is_valid_image(file_name: str, file_content: bytes):
-    file_mime = magic.from_buffer(file_content, True)
-    return file_mime.startswith("image/") and any(
-        file_name.lower().endswith(ext) for ext in ALLOWED_IMAGE_EXTENSIONS
-    )
+    # file_mime = magic.from_buffer(file_content, True)
+    # return file_mime.startswith("image/") and any(
+        # file_name.lower().endswith(ext) for ext in ALLOWED_IMAGE_EXTENSIONS
+    # )
+    return any(file_name.lower().endswith(ext) for ext in ALLOWED_IMAGE_EXTENSIONS)
