03c8eb0eb6609eada5fc07cc4e9494cd0d6bb3f8
Kishor Ramanan <kishorramanan5@gmail.com>
2024-03-31 11:17:25 +0530

    Removed LOGO in QR

--- app/utility/qr.py
+++ app/utility/qr.py
@@ -4,16 +4,7 @@ import io
 
 
 def create_qr(data: str):
-    qr = io.BytesIO()
-    segno.make_qr(data).save(qr, kind="png", scale=8)
-    qr.seek(0)
-    img = Image.open(qr)
-    img = img.convert('RGB')
-    img_width, img_height = img.size
-    logo_max_size = img_height // 3
-    logo_img = Image.open('./acm_sist_logo.png')
-    logo_img.thumbnail((logo_max_size, logo_max_size), Image.Resampling.LANCZOS)
-    box = ((img_width - logo_img.size[0]) // 2, (img_height - logo_img.size[1]) // 2)
-    img.paste(logo_img, box)
+    img = io.BytesIO()
+    segno.make_qr(data).save(img, kind="png", scale=8)
     # qr.to_artistic(background='static/Images/horizon24.gif', target=img, scale=8, kind="gif")
     return img
